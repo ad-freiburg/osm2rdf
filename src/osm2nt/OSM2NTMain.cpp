13d3220ca7d9fd79a7103da3e277c0f0ffc168c3
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2020-05-25 08:49:07 +0200

    inital commit

---
+++ src/osm2nt/OSM2NTMain.cpp
@@ -0,0 +1,7 @@
+// Copyright 2020, University of Freiburg
+// Authors: Axel Lehmann <lehmann@cs.uni-freiburg.de>.
+
+// ____________________________________________________________________________
+int main(int argc, char** argv) {
+
+}
