e303f3d045be6bb29c1d1be99a78ae5c5daf5615
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2020-07-06 11:54:16 +0200

    Unify EMPTY_TAG_SIZE handling in DumpHandler

--- src/osm2ttl/osm/DumpHandler.cpp
+++ src/osm2ttl/osm/DumpHandler.cpp
@@ -52,11 +52,10 @@ void osm2ttl::osm::DumpHandler::relation(const osmium::Relation& relation) {
   if (_config.noRelationDump) {
     return;
   }
-  if (!_config.addAreaSources &&
-      _membershipHandler->isArea(relation)) {
+  if (relation.tags().byte_size() == EMPTY_TAG_SIZE) {
     return;
   }
-  if (relation.tags().byte_size() == EMPTY_TAG_SIZE) {
+  if (!_config.addAreaSources && _membershipHandler->isArea(relation)) {
     return;
   }
   _writer->writeOsmiumRelation(relation);
@@ -67,11 +66,10 @@ void osm2ttl::osm::DumpHandler::way(const osmium::Way& way) {
   if (_config.noWayDump) {
     return;
   }
-  if (!_config.addAreaSources &&
-      _membershipHandler->isArea(way)) {
+  if (way.tags().byte_size() == EMPTY_TAG_SIZE) {
     return;
   }
-  if (way.tags().byte_size() == EMPTY_TAG_SIZE) {
+  if (!_config.addAreaSources && _membershipHandler->isArea(way)) {
     return;
   }
   _writer->writeOsmiumWay(way);
