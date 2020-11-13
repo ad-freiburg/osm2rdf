2c657a4b231529f20f6fc9dfd1518b7ee3eab8d3
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2020-11-13 10:21:52 +0100

    Simplify DirectedGraph.h headerguard

--- include/osm2ttl/util/DirectedGraph.h
+++ include/osm2ttl/util/DirectedGraph.h
@@ -1,8 +1,8 @@
 // Copyright 2020, University of Freiburg
 // Authors: Axel Lehmann <lehmann@cs.uni-freiburg.de>.
 
-#ifndef OSM2TTL_INCLUDE_OSM2TTL_UTIL_DIRECTEDGRAPH_H
-#define OSM2TTL_INCLUDE_OSM2TTL_UTIL_DIRECTEDGRAPH_H
+#ifndef OSM2TTL_UTIL_DIRECTEDGRAPH_H
+#define OSM2TTL_UTIL_DIRECTEDGRAPH_H
 
 #include <stdint.h>
 
@@ -57,4 +57,4 @@ class DirectedGraph {
 }  // namespace util
 }  // namespace osm2ttl
 
-#endif  // OSM2TTL_INCLUDE_OSM2TTL_UTIL_DIRECTEDGRAPH_H
+#endif  // OSM2TTL_UTIL_DIRECTEDGRAPH_H
