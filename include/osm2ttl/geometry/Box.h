47d3dc3fb49aec0181fa9bb5e19b0258885c2fc8
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2021-01-12 14:39:41 +0100

    Simplify osm2ttl::geometry::Box serialization

--- include/osm2ttl/geometry/Box.h
+++ include/osm2ttl/geometry/Box.h
@@ -18,19 +18,12 @@ typedef boost::geometry::model::box<osm2ttl::geometry::Location> Box;
 namespace boost {
 namespace serialization {
 template <class Archive>
-void save(Archive& ar, const osm2ttl::geometry::Box& m,
-          [[maybe_unused]] const unsigned int version) {
-  ar << boost::serialization::make_nvp("min_corner", m.min_corner());
-  ar << boost::serialization::make_nvp("max_corner", m.max_corner());
-}
-template <class Archive>
-void load(Archive& ar, osm2ttl::geometry::Box& m,
-          [[maybe_unused]] const unsigned int version) {
-  ar >> boost::serialization::make_nvp("min_corner", m.min_corner());
-  ar >> boost::serialization::make_nvp("max_corner", m.max_corner());
+void serialize(Archive& ar, osm2ttl::geometry::Box& m,
+               [[maybe_unused]] const unsigned int version) {
+  ar & boost::serialization::make_nvp("min_corner", m.min_corner());
+  ar & boost::serialization::make_nvp("max_corner", m.max_corner());
 }
 }  // namespace serialization
 }  // namespace boost
 
-BOOST_SERIALIZATION_SPLIT_FREE(osm2ttl::geometry::Box)
 #endif  // OSM2TTL_GEOMETRY_BOX_H_
