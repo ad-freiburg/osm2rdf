941db7744cfc7a79fcc4c2dae4e0804e69debc49
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2021-02-22 12:40:39 +0100

    Add _OPENMP preprocessor switch in osm2ttl::config::getInfo

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -72,7 +72,11 @@ std::string osm2ttl::config::Config::getInfo(std::string_view prefix) const {
         << prefix << "Storing statistics about geometry calculations - SLOW!";
   }
   oss << "\n" << prefix << "--- OpenMP ---";
+#if defined(_OPENMP)
   oss << "\n" << prefix << "Max Threads: " << omp_get_max_threads();
+#else
+  oss << "\n" << "Not available";
+#endif
   return oss.str();
 }
 
