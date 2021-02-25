87c1b7052be8f31eac8e6a4b1151b8a237641f8d
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2021-02-25 08:21:15 +0100

    Add basic tests for osm2ttl/util/Time.h

---
+++ tests/util/Time.cpp
@@ -0,0 +1,32 @@
+// Copyright 2020, University of Freiburg
+// Authors: Axel Lehmann <lehmann@cs.uni-freiburg.de>.
+
+#include "osm2ttl/util/Time.h"
+
+#include "gtest/gtest.h"
+
+namespace osm2ttl::util {
+
+TEST(UTIL_Time, currentTimeFormattedStructure) {
+  const std::string time = osm2ttl::util::currentTimeFormatted();
+  ASSERT_EQ(26, time.size());
+  ASSERT_EQ('[', time[0]);
+  ASSERT_EQ('-', time[5]);
+  ASSERT_EQ('-', time[8]);
+  ASSERT_EQ(' ', time[11]);
+  ASSERT_EQ(':', time[14]);
+  ASSERT_EQ(':', time[17]);
+  ASSERT_EQ('.', time[20]);
+  ASSERT_EQ(']', time[24]);
+  ASSERT_EQ(' ', time[25]);
+}
+
+TEST(UTIL_Time, formattedTimeSpacer) {
+  const std::string time = osm2ttl::util::formattedTimeSpacer;
+  ASSERT_EQ(26, time.size());
+  for (const auto& c : time) {
+    ASSERT_EQ(' ', c);
+  }
+}
+
+}
