5e57bda1e85c69b36f2e598b2f615115fcb5bbb8
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2024-01-25 12:56:52 +0100

    remove name for omp critical section for progress bar update in DAG methods

--- include/osm2rdf/util/DirectedAcyclicGraph.h
+++ include/osm2rdf/util/DirectedAcyclicGraph.h
@@ -57,7 +57,7 @@ osm2rdf::util::DirectedGraph<T> reduceDAG(
         result.addEdge(src, dst);
       }
     }
-#pragma omp critical(progress)
+#pragma omp critical
     progressBar.update(entryCount++);
   }
   progressBar.done();
@@ -96,7 +96,7 @@ osm2rdf::util::DirectedGraph<T> reduceMaximalConnectedDAG(
         result.addEdge(src, dst);
       }
     }
-#pragma omp critical(progress)
+#pragma omp critical
     progressBar.update(entryCount++);
   }
   progressBar.done();
