aadc331fb26f4ab876c73cad03205dedeab92735
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2025-02-17 16:33:18 +0100

    use new libspatialjoin

--- src/osm/GeometryHandler.cpp
+++ src/osm/GeometryHandler.cpp
@@ -62,6 +62,7 @@ GeometryHandler<W>::GeometryHandler(const osm2rdf::config::Config& config,
       _sweeper(
           {static_cast<size_t>(config.numThreads),
            static_cast<size_t>(config.numThreads),
+           10000,
            "",
            osm2rdf::ttl::constants::IRI__OPENGIS__INTERSECTS,
            osm2rdf::ttl::constants::IRI__OPENGIS__CONTAINS,
@@ -324,10 +325,10 @@ void GeometryHandler<W>::calculateRelations() {
       throw std::runtime_error("Could not read auxiliary geo file " + auxFile);
     }
 
-    ::util::JobQueue<ParseBatch> jobs(1000);             // the WKT parse jobs
+    ::util::JobQueue<sj::ParseBatch> jobs(1000);             // the WKT parse jobs
     std::vector<std::thread> thrds(_config.numThreads);  // the parse workers
     for (size_t i = 0; i < thrds.size(); i++)
-      thrds[i] = std::thread(&processQueue, &jobs, i, &_sweeper);
+      thrds[i] = std::thread(&sj::processQueue, &jobs, i, &_sweeper);
 
     ssize_t len;
     std::string dangling;
